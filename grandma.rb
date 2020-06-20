2c03eea6308f66f464983680f52ffc531462e389
Michael R <mruhaak@gmail.com>
2020-06-20 19:40:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,5 @@ def speak_to_grandma(greeting)
   		return "HUH?! SPEAK UP, SONNY!"
     elsif greeting == greeting.upcase
       return "NO, NOT SINCE 1938!"
-    else
-  		return "NO, NOT SINCE 1938!"
     end
 end
